a48d22e283fbeab9ef19c41eebfec2852b411709
yanful <yanful@uw.edu>
2023-01-26 14:59:54 -0800

    sketch1,2,4,5

--- assignment2/Sketch4-Interactive.js
+++ assignment2/Sketch4-Interactive.js
@@ -8,14 +8,38 @@ function setup() {
 }
 
 function draw() {
+
+  let x1 = 320;
+  let y1 = 200;
+  let x2 = mouseX;
+  let y2 = mouseY;
+
+  // d is the length of the line
+  // the distance from point 1 to point 2.
+  let d = dist(x1, y1, x2, y2);
+  console.log(d);
+
   background(250);
   translate(-150, 0, 0);
-  push();
-  rotateX(theta * mouseY * 0.001);
-  rotateY(theta * mouseX * 0.001);
-  //pass image as texture
-  texture(img);
-  sphere(150);
-  pop();
-  theta += 0.01;
+  if (d < 250) {
+    push();
+    rotateX(-theta * mouseY * 0.001);
+    rotateY(theta * mouseX * 0.001);
+    //pass image as texture
+    texture(img);
+    sphere(150);
+    pop();
+    theta += 0.01;
+  } else {
+    push();
+    //pass image as texture
+    texture(img);
+    sphere(150);
+    pop();
+    theta += 0.01;
+  }
+  
+  
+  
+
 }
